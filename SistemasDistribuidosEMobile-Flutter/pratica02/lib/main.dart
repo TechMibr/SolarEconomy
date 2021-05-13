400a55313c63845735ae56d1d3f970b9473b4562
Tiago Dantas <tiagodantas42@gmail.com>
2021-05-13 14:46:35 -0300

    Prática02

---
+++ SistemasDistribuidosEMobile-Flutter/pratica02/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Olá Mundo, meu nome é Tiago Dantas',
+        textDirection: TextDirection.ltr,
+      ), //Text.
+    ), //Center.
+  );
+}
